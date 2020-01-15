4cc253efae3b3b09f33f030c3a3d200751e28624
Christopher Mohr <christopher.mohr@uni-tuebingen.de>
2020-01-15 15:04:45 +0100

    Update tests, fixes #12

--- src/test/groovy/life/qbic/database/MariaDBVariantstoreStorageIntegrationTest.groovy
+++ src/test/groovy/life/qbic/database/MariaDBVariantstoreStorageIntegrationTest.groovy
@@ -1,17 +1,16 @@
 package life.qbic.database
 
 import io.micronaut.test.annotation.MicronautTest
-
-import life.qbic.oncostore.service.OncostoreStorage
+import life.qbic.oncostore.service.VariantstoreStorage
 import spock.lang.Specification
 
 import javax.inject.Inject
 
 @MicronautTest(environments=['test'])
-class MariaDBOncostoreStorageIntegrationTest extends Specification {
+class MariaDBVariantstoreStorageIntegrationTest extends Specification {
 
     @Inject
-    OncostoreStorage storage
+    VariantstoreStorage storage
 
     def "confirm that storage connection is alive"() {
         when:
