e058da978830d74a7af4c2395a77f05f1570cd42
Christopher Mohr <christopher.mohr@uni-tuebingen.de>
2021-03-05 12:18:43 +0100

    Bump version to 1.1.0-SNAPSHOT

--- src/main/groovy/life/qbic/variantstore/Variantstore.groovy
+++ src/main/groovy/life/qbic/variantstore/Variantstore.groovy
@@ -11,7 +11,7 @@ import io.swagger.v3.oas.annotations.info.License
 @OpenAPIDefinition(
         info = @Info(
                 title = "Variantstore",
-                version = "1.0.0",
+                version = "1.1.0-SNAPSHOT",
                 description = "Variantstore Restful API",
                 license = @License(name = "MIT License", url = "https://opensource.org/licenses/mit-license.php"),
                 contact = @Contact(url = "https://github.com/christopher-mohr", name = "Christopher Mohr", email = "christopher.mohr@uni-tuebingen.de")
