579af2052f37d5a7c860af901af7651209d8255d
Christopher Mohr <christopher.mohr@uni-tuebingen.de>
2021-03-24 09:32:18 +0100

    Change case class for Micronaut data

--- src/main/groovy/life/qbic/variantstore/model/Case.groovy
+++ src/main/groovy/life/qbic/variantstore/model/Case.groovy
@@ -2,6 +2,10 @@ package life.qbic.variantstore.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import groovy.transform.EqualsAndHashCode
+import io.micronaut.data.annotation.Id
+import io.micronaut.data.annotation.MappedEntity
+import io.micronaut.data.annotation.Relation
+import io.micronaut.data.model.naming.NamingStrategies
 import io.swagger.v3.oas.annotations.media.Schema
 
 /**
@@ -9,6 +13,7 @@ import io.swagger.v3.oas.annotations.media.Schema
  *
  * @since: 1.1.0
  */
+@MappedEntity(value = "entity" , namingStrategy = NamingStrategies.UnderScoreSeparatedLowerCase.class)
 @EqualsAndHashCode
 @Schema(name = "Case", description = "A case")
 class Case {
@@ -16,31 +21,45 @@ class Case {
     /**
      * The identifier of a case
      */
-    final String identifier
+    @Id
+    private final String id
     /**
-     * The project identifier associated with a case
+     * The project associated with a case
      */
-    final String projectId
+    @Relation(value = Relation.Kind.MANY_TO_ONE, mappedBy = "project_id")
+    private Project project
 
-    Case(String identifier, String projectId) {
-        this.identifier = identifier
-        this.projectId = projectId
-    }
+    /**
+     * The project associated with a case
+     */
+    @Relation(value = Relation.Kind.ONE_TO_MANY, mappedBy = "entity")
+    private final List<Sample> sample
 
-    Case(String identifier) {
-        this.identifier = identifier
-        this.projectId = ""
+    Case(String id, Project project) {
+        this.id = id
+        this.project = project
     }
 
     @Schema(description = "The case identifier")
     @JsonProperty("id")
-    String getIdentifier() {
-        return identifier
+    String getId() {
+        return id
     }
 
+    /*
     @Schema(description = "The associated project identifier")
     @JsonProperty("projectId")
     String getProjectId() {
         return projectId
+    }
+    */
+
+
+    Project getProject() {
+        return project
+    }
+
+    void setProject(Project project) {
+        this.project = project
     }
 }
